0370a702ee1e4ddbd9a74d50f1a2f2e13200694e
Kynica <dmitry.kozyuk@gmail.com>
2019-02-12 19:18:59 +0200

    Add method getId(), getName()

--- src/data/ProductData.php
+++ src/data/ProductData.php
@@ -11,4 +11,14 @@ class ProductData
     {
         $this->_productRecord = $productRecord;
     }
+
+    public function getId(): int
+    {
+        return $this->_productRecord->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->_productRecord->name;
+    }
 }
