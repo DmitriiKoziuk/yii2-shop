ad6b727617d19ece5b4464aee587f2d3c0208898
Kynica <dmitry.kozyuk@gmail.com>
2019-02-15 12:41:59 +0200

    Fix. Do not view price and buy button if product sku price not set.

--- src/views/frontend/product-sku/index.php
+++ src/views/frontend/product-sku/index.php
@@ -40,6 +40,7 @@ $this->registerLinkTag(['rel' => 'canonical', 'href' => $productSkuData->getUrl(
           <?php endif; ?>
         </div>
         <div class="col-md-6">
+          <?php if($productSkuData->isPriceOnSiteSet()): ?>
           <div class="price">
             <?= number_format(
                 $productSkuData->getPriceOnSite(),
@@ -54,6 +55,7 @@ $this->registerLinkTag(['rel' => 'canonical', 'href' => $productSkuData->getUrl(
               <?= Yii::t(ShopModule::TRANSLATION, 'Buy') ?>
             </a>
           </div>
+          <?php endif; ?>
         </div>
       </div>
     </div>
