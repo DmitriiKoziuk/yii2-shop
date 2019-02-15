cd08d61cbb188d502ac8939b58b8debcb55d498c
Kynica <dmitry.kozyuk@gmail.com>
2019-02-15 13:15:25 +0200

    Add method getMainSkuId(). Add properties type, mainSku, images, mainImage.

--- src/data/ProductData.php
+++ src/data/ProductData.php
@@ -1,10 +1,34 @@
 <?php
 namespace DmitriiKoziuk\yii2Shop\data;
 
+use DmitriiKoziuk\yii2FileManager\entities\File;
 use DmitriiKoziuk\yii2Shop\entities\Product;
 
 class ProductData
 {
+    /**
+     * @var ProductTypeData
+     */
+    public $type;
+
+    /**
+     * @var ProductSkuData
+     */
+    public $mainSku;
+
+    /**
+     * @var File[]
+     */
+    public $images;
+
+    /**
+     * @var File
+     */
+    public $mainImage;
+
+    /**
+     * @var Product
+     */
     private $_productRecord;
 
     public function __construct(Product $productRecord)
@@ -25,5 +49,10 @@ class ProductData
     public function getTypeId(): ?int
     {
         return $this->_productRecord->type_id;
+    }
+
+    public function getMainSkuId(): int
+    {
+        return $this->_productRecord->main_sku_id;
     }
 }
