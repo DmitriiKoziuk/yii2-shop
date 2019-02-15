689b05dfc7b53fb5420cbbd7b6c0d4fbbf56b390
Kynica <dmitry.kozyuk@gmail.com>
2019-02-15 10:16:54 +0200

    Add files for Russian translation

---
+++ src/messages/ru/dk-shop-cart.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'Your Cart' => 'Ваша корзина',
+    'Remove product' => 'Убрать из заказа',
+    'Update quantity' => 'Обновить количество',
+    'Checkout' => 'Оформить заказ',
+];
