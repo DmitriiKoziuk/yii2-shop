8beb6c3a76ef7791690460850c897269f410f11b
Kynica <dmitry.kozyuk@gmail.com>
2019-02-15 13:23:18 +0200

    Rewrite ProductWidget and template.

--- src/widgets/views/products.php
+++ src/widgets/views/products.php
@@ -5,9 +5,7 @@ use DmitriiKoziuk\yii2FileManager\helpers\FileWebHelper;
 
 /**
  * @var $this \yii\web\View
- * @var $products \DmitriiKoziuk\yii2Shop\entities\Product[]
- * @var $productImages \DmitriiKoziuk\yii2FileManager\entities\File[]
- * @var $productMainImages \DmitriiKoziuk\yii2FileManager\entities\File[]
+ * @var $products \DmitriiKoziuk\yii2Shop\data\ProductData[]
  * @var $pagination \yii\data\Pagination
  * @var $fileWebHelper FileWebHelper
  */
@@ -23,12 +21,12 @@ ProductWidgetAsset::register($this);
         <div class="row">
           <div class="col-md-12">
             <div class="image">
-              <a href="<?= $product->getMainSku()->url ?>">
-                  <?php if (! empty($productMainImages[ $product->getMainSku()->id ])): ?>
-                    <img src="<?= $fileWebHelper->getFileFullWebPath(
-                        $productMainImages[ $product->getMainSku()->id ]
-                    ) ?>" alt="">
-                  <?php endif; ?>
+              <a href="<?= $product->mainSku->getUrl() ?>">
+                <?php if (! empty($product->mainImage)): ?>
+                <img src="<?= $fileWebHelper->getFileFullWebPath(
+                    $product->mainImage
+                ) ?>" alt="">
+                <?php endif; ?>
               </a>
             </div>
           </div>
@@ -36,9 +34,9 @@ ProductWidgetAsset::register($this);
         <div class="row">
           <div class="col-md-12">
             <div class="title">
-              <a href="<?= $product->getMainSku()->url ?>">
-                <span class="type-name"><?= $product->getTypeName() ?></span>
-                  <?= $product->name ?> <?= $product->getMainSku()->name ?>
+              <a href="<?= $product->mainSku->getUrl() ?>">
+                <span class="type-name"><?= $product->type->getName() ?></span>
+                  <?= $product->getName() ?> <?= $product->mainSku->getName() ?>
               </a>
             </div>
           </div>
@@ -49,23 +47,27 @@ ProductWidgetAsset::register($this);
               <div class="row align-items-center">
                 <div class="col-md-6">
                   <div class="price">
+                    <?php if ($product->mainSku->isPriceOnSiteSet()): ?>
                     <?= number_format(
-                        $product->getMainSku()->price_on_site,
+                        $product->mainSku->getPriceOnSite(),
                         0,
                         '.',
                         ' '
                     ) ?>
                     <span class="currency">currency</span>
+                    <?php endif; ?>
                   </div>
                 </div>
                 <div class="col-md-6">
-                  <a class="btn buy-button" href="/cart/add-product?product=<?= $product->getMainSku()->id ?>">
+                  <?php if ($product->mainSku->isPriceOnSiteSet()): ?>
+                  <a class="btn buy-button" href="/cart/add-product?product=<?= $product->mainSku->getId() ?>">
                     Buy
                   </a>
-                  <a class="btn favorite-button" href="/favorite/add?product=<?= $product->getMainSku()->id ?>">
+                  <?php endif; ?>
+                  <a class="btn favorite-button" href="/favorite/add?product=<?= $product->mainSku->getId() ?>">
                     F
                   </a>
-                  <a class="btn compare-button" href="/compare/add?product=<?= $product->getMainSku()->id ?>">
+                  <a class="btn compare-button" href="/compare/add?product=<?= $product->mainSku->getId() ?>">
                     C
                   </a>
                 </div>
