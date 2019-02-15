c2fd80200f3138e5508a7e0a41f6384b6e47842f
Kynica <dmitry.kozyuk@gmail.com>
2019-02-15 10:24:33 +0200

    Add translation

--- src/messages/en/dk-shop-cart.php
+++ src/messages/en/dk-shop-cart.php
@@ -4,4 +4,8 @@ return [
     'Remove product' => 'Remove product',
     'Update quantity' => 'Update quantity',
     'Checkout' => 'Checkout',
+    'First name' => 'First name',
+    'Middle name' => 'Middle name',
+    'Last name' => 'Last name',
+    'Phone number' => 'Phone number',
 ];
--- src/messages/ru/dk-shop-cart.php
+++ src/messages/ru/dk-shop-cart.php
@@ -4,4 +4,8 @@ return [
     'Remove product' => 'Убрать из заказа',
     'Update quantity' => 'Обновить количество',
     'Checkout' => 'Оформить заказ',
+    'First name' => 'Имя',
+    'Middle name' => 'Отчество',
+    'Last name' => 'Фамилия',
+    'Phone number' => 'Номер телефона',
 ];
