80f5b1f266eced3b28eeb053fae26f4a7504c0fd
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2019-10-15 14:06:45 +0300

    Add _fixtures() Fix testCreateRootCategory()

--- tests/unit/services/category/CategoryServiceCreateCategoryTest.php
+++ tests/unit/services/category/CategoryServiceCreateCategoryTest.php
@@ -5,6 +5,7 @@ namespace DmitriiKoziuk\yii2Shop\tests;
 use Yii;
 use yii\di\Container;
 use DmitriiKoziuk\yii2UrlIndex\entities\UrlEntity;
+use DmitriiKoziuk\yii2Shop\tests\_fixtures\CategoryFixture;
 use DmitriiKoziuk\yii2Shop\forms\CategoryInputForm;
 use DmitriiKoziuk\yii2Shop\services\category\CategoryService;
 use DmitriiKoziuk\yii2Shop\entities\Category;
@@ -16,27 +17,35 @@ class CategoryServiceCreateCategoryTest extends \Codeception\Test\Unit
      */
     protected $tester;
 
+    public function _fixtures()
+    {
+        return [
+            'category' => [
+                'class' => CategoryFixture::class,
+                'dataFile' => codecept_data_dir() . 'category_data.php'
+            ],
+        ];
+    }
 
     protected function _after()
     {
         Yii::$container = new Container();
     }
 
-
     public function testCreateRootCategory()
     {
         /** @var CategoryService $categoryService */
         $categoryService = Yii::$container->get(CategoryService::class);
         $this->assertInstanceOf(CategoryService::class, $categoryService);
-        $this->tester->dontSeeRecord(Category::class, ['name' => 'Electronics']);
         $categoryAttributes = [
             'name' => 'Some root category name',
         ];
+        $this->tester->dontSeeRecord(Category::class, ['name' => $categoryAttributes['name']]);
         $categoryInputForm = new CategoryInputForm($categoryAttributes);
         $this->assertTrue($categoryInputForm->validate());
         $createdCategory = $categoryService->createCategory($categoryInputForm);
         $this->assertInstanceOf(Category::class, $createdCategory);
-        $this->tester->seeRecord(Category::class, $categoryAttributes);
+        $this->tester->seeRecord(Category::class, ['name' => $categoryAttributes['name']]);
         $this->tester->seeRecord(UrlEntity::class, ['url' => $createdCategory->url]);
     }
 }
