914829e0face2f5d31b437e9b96f2936fa0b67a6
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2019-11-28 16:00:31 +0200

    Rewrite productWidget Link pager view. Use bootstrap.

--- src/widgets/frontend/views/link-pager.php
+++ src/widgets/frontend/views/link-pager.php
@@ -6,12 +6,16 @@
 
 ?>
 
-<?php foreach ($buttons as $button): ?>
-
-  <?php if ($button['active']): ?>
-  <span><?= $button['label'] ?></span>
-  <?php else: ?>
-  <a href="<?= $button['url'] ?>"><?= $button['label'] ?></a>
-  <?php endif; ?>
-
-<?php endforeach; ?>
+<nav>
+  <ul class="pagination">
+    <?php foreach ($buttons as $button): ?>
+    <li class="<?= $button['active'] == true ? 'active' : '' ?>">
+      <?php if ($button['active']): ?>
+      <span><?= $button['label'] ?></span>
+      <?php else: ?>
+      <a href="<?= $button['url'] ?>"><?= $button['label'] ?></a>
+      <?php endif; ?>
+    </li>
+    <?php endforeach; ?>
+  </ul>
+</nav>
