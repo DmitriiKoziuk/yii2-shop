e17d5ece77156c5ad4d624250b4ff480f456349d
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2020-01-12 14:38:21 +0200

    Fix error int attribute name.

--- src/entities/search/ProductTypeAttributeEntitySearch.php
+++ src/entities/search/ProductTypeAttributeEntitySearch.php
@@ -68,7 +68,7 @@ class ProductTypeAttributeEntitySearch extends ProductTypeAttributeEntity
         $query->andFilterWhere([
             'product_type_id' => $this->product_type_id,
             'attribute_id' => $this->attribute_id,
-            'view_attribute_at_product_preview' => $this->view_attribute_at_product_sku_preview,
+            'view_attribute_at_product_sku_preview' => $this->view_attribute_at_product_sku_preview,
             'sort_at_product_sku_preview' => $this->sort_at_product_sku_preview,
         ]);
 
