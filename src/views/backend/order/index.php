8a0a5b45a7ef685c6abdccf0de024c0648f2a349
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2020-02-12 16:50:44 +0200

    Remove unnecessary views.

--- src/views/backend/order/index.php
+++ src/views/backend/order/index.php
@@ -31,10 +31,6 @@ OrderIndexAsset::register($this);
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a('Create Order', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
     <?php Pjax::begin(); ?>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
